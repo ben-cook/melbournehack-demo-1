b9f567ca9695359f1b310e6beac5da8b3821f63c
Ben Cook <mcbeanoo@gmail.com>
2021-08-14 01:30:32 +1000

    create static list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ul>
+          <li>Eggs</li>
+          <li>Milk</li>
+          <li>Chocolate</li>
+          <li>Chips</li>
+          <li>Fruit</li>
+        </ul>
+        <p>You have 5 items in your shopping list</p>
+        <input></input>
+        <button>Add item</button>
+      </div>
     </div>
   );
 }
